76294c5ebbdf1fcd4bebdcea1f205a1bb15e6410
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-22 19:05:32 +0100

    updates

---
+++ imports.py
@@ -0,0 +1,14 @@
+#imports in python
+
+import random
+
+# type_of_drinks = ["soda", "coffee", "tea"]
+# print(random.choice(type_of_drinks))
+
+# print(random.randint(1, 20))  #range from 1 to 20
+
+
+import math
+
+square_root = math.sqrt(64)
+print(square_root)
