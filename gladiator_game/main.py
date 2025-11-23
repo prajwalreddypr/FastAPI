025a8f7f1c9212dab17ef1b4a4da888c9f2b8647
Prajwal Reddy <reddyprajwal2000@gmail.com>
2025-11-23 10:25:58 +0100

    Oops concepts c'est finis

--- gladiator_game/main.py
+++ gladiator_game/main.py
@@ -13,43 +13,81 @@
 from Enemy import *
 from Zombie import *
 from Ogre import *
+from Hero import *
+from Weapon import *
 
 #multiple objects of enemy
-zombie = Zombie( 15, 3)
-ogre = Ogre(20, 4)
+# zombie = Zombie( 15, 3)
+# ogre = Ogre(20, 4)
 # big_zombie = Enemy("Big Zombie", 30, 5)
 # zombie.__type_of_enemy = 'Ogre'
 # print(zombie.get_type_of_enemy())
 
-def battle(e1: Enemy, e2: Enemy):
-    e1.talk()
-    e2.talk()
+# def battle(e1: Enemy, e2: Enemy):
+#     e1.talk()
+#     e2.talk()
     
-    while e1.health_points > 0 and e2.health_points > 0:
-      print("--------------")
-      e1.special_attack()
-      e2.special_attack()
+#     while e1.health_points > 0 and e2.health_points > 0:
+#       print("--------------")
+#       e1.special_attack()
+#       e2.special_attack()
       
-      print(f"{e1.get_type_of_enemy()}: {e1.health_points} HP left.")
-      print(f"{e2.get_type_of_enemy()}: {e2.health_points} HP left.")
+#       print(f"{e1.get_type_of_enemy()}: {e1.health_points} HP left.")
+#       print(f"{e2.get_type_of_enemy()}: {e2.health_points} HP left.")
       
-      e2.attack()
-      e1.health_points -= e2.attack_damage
-      e1.attack()
-      e2.health_points -= e1.attack_damage
+#       e2.attack()
+#       e1.health_points -= e2.attack_damage
+#       e1.attack()
+#       e2.health_points -= e1.attack_damage
       
-      print("------------------")
+#       print("------------------")
       
       
-      if e1.health_points > 0:
-          print(f"{e1.get_type_of_enemy()} wins !")
-      else:
-          print(f"{e2.get_type_of_enemy()} wins !") 
+#       if e1.health_points > 0:
+#           print(f"{e1.get_type_of_enemy()} wins !")
+#       else:
+#           print(f"{e2.get_type_of_enemy()} wins !") 
           
-zombie = Zombie(10, 1)
-ogre = Ogre(20, 3) 
+# zombie = Zombie(10, 1)
+# ogre = Ogre(20, 3) 
 
-battle(zombie, ogre)
+# battle(zombie, ogre)
+
+
+def hero_battle(hero: Hero, enemy: Enemy):
+    
+    while hero.health_points > 0 and enemy.health_points > 0:
+        
+        print("--------------")
+        enemy.special_attack()
+        enemy.attack()
+        hero.health_points -= enemy.attack_damage
+        
+        hero.attack()
+        enemy.health_points -= hero.attack_damage
+        
+        print("--------------")
+        
+        if hero.health_points > 0:
+            print("Hero wins")
+        else:
+            print("ENemy wins")
+
+zombie = Zombie(10, 1)
+hero = Hero(10, 1)
+weapon = Weapon('Sword', 5)
+hero.weapon = weapon
+hero.equip_weapon()
+hero_battle(hero, zombie)
+        
+    
+    
+    
+    
+    
+    
+    
+    
 
 # battle(zombie)
 # battle(ogre)
