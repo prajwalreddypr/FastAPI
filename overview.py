e7c65f920d604351302cb5b47f40e8b9dbeb7e8c
Prajwal Reddy <reddyprajwal2000@gmail.com>
2026-01-23 12:07:37 +0530

    Resuming FastAPI project

---
+++ overview.py
@@ -0,0 +1,24 @@
+'''
+FastAPI is fast(to build) and FastAPI is fast(to run/perform)
+
+
+WEbframework to build modern RestFULL API's
+
+Few bugs
+QUick and Easy
+Robust, production standards
+
+Pip is python package installation manager
+
+
+Python already has an inclluded dependency for Virtual Env called venv.
+
+#HTTP Request methods
+CRUD operations
+
+Create -> POST
+Read -> GET
+Update -> PUT
+Delete -> DELETE
+
+'''
